039c3aa551143164db5d902c4c5d073cf62ea40f
ManuelGallegos <manuel_mty@yahoo.com>
2018-01-25 14:56:50 -0500

    Add project files.

---
+++ AndConditions/AndConditions/AndConditionsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndConditions
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AndConditionsPage : ContentPage
+    {
+        public AndConditionsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
